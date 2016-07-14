5c606d5e4e6fc1e5cfbd2462077ab3e2dc8f1146
Dominic Bisset <mail@dominicbisset.co.uk>
2016-07-14 17:28:19 +0100

    Improved layout algorithm for results.  Now works well for two different printed result types

--- app/components/playbookResult.component.ts
+++ app/components/playbookResult.component.ts
@@ -4,8 +4,8 @@ import GBGameModels from "gb-game-models";
 @Component({
     selector: '[playbookResult]',
     template: `
-        <svg:circle cx="50" cy="50" r="45" stroke="green" stroke-width="1" [attr.fill]="isMomentous ? 'green' : 'yellow'" />
-        <svg:text *ngFor="let effect of result.effects; let i = index" [attr.x]="25" [attr.y]="((i + 1) * 25) + 10" font-size="35">{{effectText(effect)}}</text>
+        <svg:circle cx="50" cy="50" r="45" stroke="darkgrey" stroke-width="1" [attr.fill]="isMomentous ? 'yellow' : 'white'" />
+        <svg:text *ngFor="let effect of result.effects; let i = index" text-anchor="middle" [attr.x]="textX(i)" [attr.y]="textY(i)" font-size="35">{{effectText(effect)}}</text>
         `,
 })
 export class PlaybookResult {
@@ -13,6 +13,36 @@ export class PlaybookResult {
     isMomentous: boolean;
     ngOnInit() {
         this.isMomentous = this.result.effects.find(re => re.resultType === GBGameModels.ResultType.Momentous) !== undefined;
+    }
+
+    textX(index: number): number{
+        let trueCount: number = this.result.effects.length;
+        let trueIndex: number = index;
+        if (this.isMomentous) {
+            trueCount -= 1;
+            trueIndex -= 1;
+        }
+        if (trueCount < 3) {
+            return 50;
+        }
+        else {
+            return ((trueIndex % 2) * 25) + 12.5;
+        }
+    }
+
+    textY(index: number): number {
+        let trueCount: number = this.result.effects.length;
+        let trueIndex: number = index;
+        if (this.isMomentous) {
+            trueCount -= 1;
+            trueIndex -= 1;
+        }
+        if (trueCount === 1) {
+            return 62;
+        }
+        else {
+            return (Math.floor((trueIndex + 1) / 2)  * 35) + 42;
+        }
     }
 
     effectText(effect: GBGameModels.ResultEffect): string {
